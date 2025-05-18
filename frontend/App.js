efb290d5789b72d79aad8661db4d2f4ba7801514
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-05-18 12:38:42 +0530

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import Home from "./pages/Home";
+import PostDetails from "./pages/PostDetails";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
+
+const PrivateRoute = ({ element }) => {
+  const { user } = useAuth();
+  return user ? element : <Navigate to="/login" />;
+};
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<PostDetails />} />
+          <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
